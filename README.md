4cf4f70d9af0228ace5a2fea6ac8176ab070108b
gary <gary.white@lodgit.com>
2021-04-12 17:47:25 +0200

    code

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ From the book Headfirst Javascript.
 
 handler.html -> reveals handler
 
-![handler image](media/handler.png)
+![handler image](handler.png)
 
 imageGuess.html -> Click a blurred image to reveal true image
 
-![blur image](media/blur.png)
-![blur image 2](media/blur2.png)
+![blur image](blur.png)
+![blur image 2](blur2.png)
--- imageGuess.js
+++ imageGuess.js
@@ -1,11 +1,20 @@
 
 window.onload = init;
 function init() {
-    var image = document.getElementById("zero");
-    image.onclick = showAnswer;
+    // var image = document.getElementById("zero");
+    // image.onclick = showAnswer;
+    var images = document.getElementsByTagName("img");
+    for (var i = 0; i < images.length; i++) {
+        images[i].onclick = showAnswer;
+    }
 }
 
-function showAnswer() {
-    var image = document.getElementById("zero");
-    image.src = "/media/zero.jpg";
+function showAnswer(eventObj) {
+    // var image = document.getElementById("zero");
+    // image.src = "/media/zero.jpg";
+    var image = eventObj.target;
+
+    var name = image.id;
+    name = name + ".jpg";
+    image.src = name;
 }
